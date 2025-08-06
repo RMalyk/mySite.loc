a8bfef8b467aed62fcbd85f4753ae70354c04367
RMalyk <82038607+RMalyk@users.noreply.github.com>
2025-08-06 19:09:05 +0300

    Start project

---
+++ wp-content/themes/mysite/gulpfile.js
@@ -0,0 +1,167 @@
+let project_folder = "assets";
+let source_folder = "src";
+
+
+let path = {
+    biuld: {
+        html: project_folder + "/",
+        css: project_folder + "/css/",
+        js: project_folder + "/js/",
+        images: project_folder + "/images/",
+        video: project_folder + "/video/",
+        fonts: project_folder + "/fonts/"
+    },
+    src: {
+        html: [source_folder + "/*.html", "!" + source_folder + "/_*.html"],
+        css: [
+            source_folder + "/scss/*.scss",
+            // source_folder + "/scss/content-single-product.scss",
+        ],
+        js: [
+            source_folder + "/js/libs/*.js",
+            source_folder + "/js/*.js",
+        ],
+        images: source_folder + "/images/**/*",
+        video: source_folder + "/video/**/*",
+        fonts: source_folder + "/fonts/**/*"
+    },
+    watch: {
+        html: source_folder + "/**/*.html",
+        css: source_folder + "/scss/**/*.scss",
+        js: source_folder + "/js/**/*.js",
+        images: source_folder + "/images/**/*"
+    },
+    clean: "./" + project_folder + "/"
+}
+
+let { src, dest } = require('gulp'),
+    gulp = require('gulp'),
+    fileinclude = require('gulp-file-include'),
+    del = require('del'),
+    scss = require('gulp-sass')(require('sass')),
+    autoprefixer = require('gulp-autoprefixer'),
+    gulp_media = require('gulp-group-css-media-queries'),
+    clean_css = require('gulp-clean-css'),
+    rename = require('gulp-rename'),
+    imagemin = require('gulp-imagemin'),
+    webp = require('gulp-webp'),
+    webp_html = require('gulp-webp-html'),
+    // webp_css = require('gulp-webp-css'),
+    woff2 = require('gulp-ttf2woff2'),
+    woff = require('gulp-ttf2woff'),
+    uglify = require('gulp-uglify-es').default,
+    browserSync = require('browser-sync').create();
+concat = require('gulp-concat');
+
+
+function browsersync() {
+    browserSync.init({
+        server: {
+            baseDir: "./" + project_folder + "/"
+        },
+        port: 3000,
+        notify: false
+    })
+}
+
+function html() {
+    return src(path.src.html)
+        .pipe(fileinclude(
+            {
+                prefix: '@@',
+                basepath: 'src/'
+            }
+        ))
+        // .pipe(webp_html())
+        .pipe(dest(path.biuld.html))
+        .pipe(browserSync.stream())
+}
+
+function img() {
+    return src(path.src.images)
+        .pipe(webp({
+            quality: 70
+        }))
+        .pipe(dest(path.biuld.images))
+        .pipe(src(path.src.images))
+        .pipe(imagemin({
+            interlaced: true,
+            progressive: true,
+            optimizationLevel: 3,
+            svgoPlugins: [{ removeViewBox: false }]
+        }))
+        .pipe(dest(path.biuld.images))
+        .pipe(browserSync.stream())
+}
+
+function video() {
+    return src(path.src.video)
+        .pipe(dest(path.biuld.video))
+        .pipe(browserSync.stream())
+}
+
+function js() {
+    return src(path.src.js)
+        // .pipe(fileinclude())
+        .pipe(concat('script.js'))
+        .pipe(dest(path.biuld.js))
+        .pipe(uglify())
+        .pipe(rename({
+            extname: ".min.js"
+        }))
+        .pipe(dest(path.biuld.js))
+        .pipe(browserSync.stream())
+}
+
+function css() {
+    return src(path.src.css)
+        .pipe(scss({
+            outputStyle: "expanded"
+        }))
+        .pipe(autoprefixer({
+            overrideBrowserslist: ["last 5 versions"],
+            cascade: true
+        }))
+        .pipe(gulp_media())
+        // .pipe(webp_css())
+        .pipe(dest(path.biuld.css))
+        .pipe(clean_css())
+        .pipe(rename({
+            extname: ".min.css"
+        }))
+        .pipe(dest(path.biuld.css))
+        .pipe(browserSync.stream())
+}
+
+
+function watchFilles() {
+    gulp.watch([path.watch.html], html),
+        gulp.watch([path.watch.css], css),
+        gulp.watch([path.watch.js], js),
+        gulp.watch([path.watch.images], img)
+}
+
+function clean() {
+    return del(path.clean)
+}
+
+function fonts() {
+    src(path.src.fonts)
+        .pipe(woff())
+        .pipe(dest(path.biuld.fonts))
+    return src(path.src.fonts)
+        .pipe(woff2())
+        .pipe(dest(path.biuld.fonts))
+}
+
+let build = gulp.series(gulp.parallel(js, css, html, img, video, fonts))
+let watch = gulp.parallel(build, watchFilles, browsersync);
+
+exports.js = js;
+exports.img = img;
+exports.css = css;
+exports.html = html;
+exports.watch = watch;
+exports.fonts = fonts;
+exports.build = build;
+exports.default = watch;
---
+++ wp-content/themes/mysite/src/js/script.js
@@ -0,0 +1,12 @@
+jQuery(function( $ ) {
+
+		$('.bg_slider').slick({
+			arrows: false,
+			infinite: true,
+			autoplay: true,
+			autoplaySpeed: 2000,
+	});
+
+});
+
+
