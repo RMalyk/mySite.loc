8864876eca48e6d83902a0196b320eb06ec054ce
RMalyk <82038607+RMalyk@users.noreply.github.com>
2025-08-08 10:43:48 +0300

    Added first demo

--- wp-content/themes/mysite/single.php
+++ wp-content/themes/mysite/single.php
@@ -1,40 +1,21 @@
 <?php
-/**
- * The template for displaying all single posts
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
- *
- * @package mySite
- */
 
 get_header();
+
+$demo_path = get_field('demo_link');
+$demo_url  = get_template_directory_uri() . $demo_path;
+
 ?>
 
-	<main id="primary" class="site-main">
+<main class="portfolio-page">
+	<iframe
+		src="<?php echo esc_url($demo_url); ?>"
+		width="100%"
+		height="800"
+		style="border: none;"
+		loading="lazy"
+		title="Демо роботи"
+	></iframe>
+</main>
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
-
-			get_template_part( 'template-parts/content', get_post_type() );
-
-			the_post_navigation(
-				array(
-					'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'mysite' ) . '</span> <span class="nav-title">%title</span>',
-					'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'mysite' ) . '</span> <span class="nav-title">%title</span>',
-				)
-			);
-
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
-		endwhile; // End of the loop.
-		?>
-
-	</main><!-- #main -->
-
-<?php
-get_sidebar();
-get_footer();
+<?php get_footer(); ?>
